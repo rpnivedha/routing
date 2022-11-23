222299a10ca484097c49289b668e7f647278121a
rpnivedha <rpnivedha16@gmail.com>
2022-11-23 22:31:47 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { CurrentdateComponent } from './currentdate/currentdate.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurrentdateComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AdminModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
